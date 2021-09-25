1d5ac793f2a1b31ceaf5aabb8c7c36a1c6881706
Kano Marvel <knmarvel@gmail.com>
2021-09-25 12:17:50 -0400

    changed logging to python logging module

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import logging
 import sys
 import time
 
+# set up logging configuration
+# from https://realpython.com/python-logging/#basic-configurations
+logging.basicConfig(
+    level="DEBUG",
+    filename='greendays.log',
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+
 def main(*args):
+    # start up
     start_time = time.time()
+    logging.info("Starting up")
     print(f"Starting up...{time.ctime(start_time)}")
+
     print(f"Running")
+
+    # end
     end_time = time.time()
+    logging.info(f"Stopping. Time elapsed: {end_time - start_time} seconds")
     print(f"Stopping...{time.ctime(end_time)}\nTime elapsed: {end_time - start_time} seconds")
 
 
